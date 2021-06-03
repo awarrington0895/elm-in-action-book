11c0f70c6b53af7b266f4b36f6aa9aec3b8d8ca2
awarrington0895 <awarrington0895@gmail.com>
2021-06-03 13:57:56 +0000

    Fix bug associated with filter values type

--- src/Main.elm
+++ src/Main.elm
@@ -114,7 +114,7 @@ port setFilters : FilterOptions -> Cmd msg
 
 type alias FilterOptions =
     { url : String
-    , filters : List { name : String, amount : Int }
+    , filters : List { name : String, amount : Float }
     }
 
 
@@ -221,9 +221,9 @@ applyFilters model =
         Loaded _ selectedUrl ->
             let
                 filters =
-                    [ { name = "Hue", amount = model.hue }
-                    , { name = "Ripple", amount = model.ripple }
-                    , { name = "Noise", amount = model.noise }
+                    [ { name = "Hue", amount = toFloat model.hue / 11 }
+                    , { name = "Ripple", amount = toFloat model.ripple / 11 }
+                    , { name = "Noise", amount = toFloat model.noise / 11 }
                     ]
 
                 url =
